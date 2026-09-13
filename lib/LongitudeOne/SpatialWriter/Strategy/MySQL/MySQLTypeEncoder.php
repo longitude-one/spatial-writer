579b1c3ebd66ce5b05859eed2927d72fe86fd61d
Alexandre Tranchant <alexandre.tranchant@gmail.com>
2026-09-13 14:31:18 +0200

    refactor: Replace TypeEnum with GeometryTypeEnum in binary strategy classes

--- lib/LongitudeOne/SpatialWriter/Strategy/MySQL/MySQLTypeEncoder.php
+++ lib/LongitudeOne/SpatialWriter/Strategy/MySQL/MySQLTypeEncoder.php
@@ -16,7 +16,7 @@ declare(strict_types=1);
 
 namespace LongitudeOne\SpatialWriter\Strategy\MySQL;
 
-use LongitudeOne\SpatialTypes\Enum\TypeEnum;
+use LongitudeOne\Core\Enum\GeometryTypeEnum;
 use LongitudeOne\SpatialTypes\Interfaces\SpatialInterface;
 use LongitudeOne\SpatialWriter\Exception\UnsupportedSpatialTypeException;
 
@@ -37,14 +37,14 @@ class MySQLTypeEncoder
     public function writeType(SpatialInterface $spatial): string
     {
         return match ($spatial->getType()) {
-            TypeEnum::POINT->value => pack('L', 1),
-            TypeEnum::LINESTRING->value => pack('L', 2),
-            TypeEnum::POLYGON->value => pack('L', 3),
-            TypeEnum::MULTIPOINT->value => pack('L', 4),
-            TypeEnum::MULTILINESTRING->value => pack('L', 5),
-            TypeEnum::MULTIPOLYGON->value => pack('L', 6),
-            TypeEnum::COLLECTION->value => pack('L', 7),
-            default => throw new UnsupportedSpatialTypeException(sprintf('MySQL adapter does not support spatial type %s', $spatial->getType())),
+            GeometryTypeEnum::POINT => pack('L', 1),
+            GeometryTypeEnum::LINESTRING => pack('L', 2),
+            GeometryTypeEnum::POLYGON => pack('L', 3),
+            GeometryTypeEnum::MULTIPOINT => pack('L', 4),
+            GeometryTypeEnum::MULTILINESTRING => pack('L', 5),
+            GeometryTypeEnum::MULTIPOLYGON => pack('L', 6),
+            GeometryTypeEnum::GEOMETRYCOLLECTION => pack('L', 7),
+            default => throw new UnsupportedSpatialTypeException(sprintf('MySQL adapter does not support spatial type %s', $spatial->getType()->value)),
         };
     }
 }
